a66c94627be29cd5895516d7440c273eec105769
zyx1025 <109834311+zyx1025@users.noreply.github.com>
2024-08-17 16:27:36 +0800

    lab3 100%

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -28,19 +28,42 @@ public class TestBuggyAList {
     @Test
     public void randomizedTest(){
         AListNoResizing<Integer> L = new AListNoResizing<>();
-
-        int N = 500;
+        BuggyAList<Integer> buggyAList = new BuggyAList<>();
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
+            int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
+
                 L.addLast(randVal);
+                buggyAList.addLast(randVal);
+
                 System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
-                int size = L.size();
-                System.out.println("size: " + size);
+                assertEquals(L.size(),buggyAList.size());
+            }else if (operationNumber == 2){
+                //getLast
+                int last1 = -1;
+                int last2 = -1;
+                if(L.size() > 0){
+                    last1 = L.getLast();
+                }
+                if ( buggyAList.size()> 0 ){
+                    last2 = buggyAList.getLast();
+                }
+                if (last1!=-1 && last2!=-1){
+                    assertEquals(last1,last2);
+                }
+            }else{
+                //removeLast
+                if(L.size() > 0){
+                    L.removeLast();
+                }
+                if(buggyAList.size() > 0){
+                    buggyAList.removeLast();
+                }
             }
         }
     }
